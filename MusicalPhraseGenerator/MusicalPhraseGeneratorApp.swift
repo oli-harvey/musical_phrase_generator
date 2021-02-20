0e1733acb8bcd001f056ef23e127ae80aa4681d7
Oliver Harvey <oliharvey@hotmail.com>
2021-02-20 18:25:39 +0000

    Initial Commit

---
+++ MusicalPhraseGenerator/MusicalPhraseGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicalPhraseGeneratorApp.swift
+//  MusicalPhraseGenerator
+//
+//  Created by Oliver Harvey on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MusicalPhraseGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
