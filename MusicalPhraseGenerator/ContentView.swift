96bbefe788511b2e853be72892058d39e1fa01bf
Oliver Harvey <oliharvey@hotmail.com>
2021-03-06 19:33:10 +0000

    decided to not use images and generate notation instead

--- MusicalPhraseGenerator/ContentView.swift
+++ MusicalPhraseGenerator/ContentView.swift
@@ -8,14 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    var notes = Note.fanfare()
+    var test = [
+        1,2,3
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            ForEach(notes) { note in
+                note.duration.notation()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Note: Identifiable, Hashable {
+    var id = UUID()
+    let pitch: Pitch
+    let accidental: Accidental
+    let duration: Duration
+}
+
+extension Note {
+    static func fanfare() -> [Note] {
+        return [
+            Note(
+                pitch: .c,
+                accidental: .natural,
+                duration: .sixteenth),
+            Note(
+                pitch: .d,
+                accidental: .natural,
+                duration: .sixteenth),
+            Note(
+                pitch: .a,
+                accidental: .natural,
+                duration: .sixteenth),
+            Note(
+                pitch: .a,
+                accidental: .natural,
+                duration: .sixteenth),
+            Note(
+                pitch: .a,
+                accidental: .natural,
+                duration: .sixteenth)
+        ]
+    }
+}
+
+enum Pitch {
+    case a, b, c, d, e, f, g, x
+}
+
+enum Accidental {
+    case sharp, flat, natural
+        
+    func image() -> some View {
+        switch self {
+        case .sharp:
+            return Text("#")
+        case .flat:
+            return Text("b")
+        case .natural:
+            return Text("")
+        }
+    }
+}
+
+enum Duration {
+    case whole, half, quarter, eighth, sixteenth
+    func notation() -> some View {
+        switch self {
+        case .whole:
+                return Circle()
+        case .half:
+                return Circle()
+        case .quarter:
+                return Circle()
+        case .eighth:
+                return Circle()
+        case .sixteenth:
+                return Circle()
+        }
     }
 }
